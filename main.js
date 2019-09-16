79efad9c51acb46908a17c67a6cadb233963bf7f
Ivan Phelps <ivanniothemannio@gmail.com>
2019-09-16 18:28:28 -0500

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("main.js is linked");
+
+const toCelsius = (fahrenheit) => {
+    let offset = fahrenheit - 32;
+    let converted = offset * (5/9);
+    console.log(Math.round(converted * 100) / 100);
+}
+
+const toFahrenheit = (celsius) => {
+    let offset = celsius * (9/5);
+    let converted = offset + 32;
+    console.log(Math.round(converted * 100) / 100);
+}
+
