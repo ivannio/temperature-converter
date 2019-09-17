b473613052e59d63b834e2622d332d06d132d28a
Ivan Phelps <ivanniothemannio@gmail.com>
2019-09-16 21:04:32 -0500

    convert button works

--- main.js
+++ main.js
@@ -3,12 +3,27 @@ console.log("main.js is linked");
 const toCelsius = (fahrenheit) => {
     let offset = fahrenheit - 32;
     let converted = offset * (5/9);
-    console.log(Math.round(converted * 100) / 100);
+    document.getElementById("temp-p").innerHTML = Math.round(converted * 100) / 100;
 }
 
 const toFahrenheit = (celsius) => {
     let offset = celsius * (9/5);
     let converted = offset + 32;
-    console.log(Math.round(converted * 100) / 100);
+    document.getElementById("temp-p").innerHTML = Math.round(converted * 100) / 100;
 }
 
+const convert = (temp) => {
+    let fChecked = document.getElementById("fahrenheit-radio").checked;
+    let cChecked = document.getElementById("celsius-radio").checked;
+    if (fChecked) {
+        toFahrenheit(temp);
+    } else if (cChecked) {
+        toCelsius(temp);
+    } else {
+        alert("Please select which format to convert to");
+    }
+}
+
+document.getElementById("convert-button").addEventListener("click", () => {
+    convert(document.getElementById("temp-to-convert").value);
+  });
